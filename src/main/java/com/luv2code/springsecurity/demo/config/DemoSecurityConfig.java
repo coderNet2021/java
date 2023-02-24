b3f80a8c5fec17622d3661f1f9d6d2067460f6d2
coderNet2021 <cnss.saida@gmail.com>
2023-02-24 13:01:36 +0100

    the spring framework is now using the custom login form that we precise

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,10 +2,12 @@ package com.luv2code.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -36,4 +38,30 @@ public class DemoSecurityConfig {
 	        
 	    }
 
+	 @Bean
+		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+			
+			return http
+				.authorizeRequests(configurer -> 
+					configurer
+						.antMatchers("/").hasRole("EMPLOYEE")
+						.antMatchers("/leaders/**").hasRole("MANAGER")
+						.antMatchers("/systems/**").hasRole("ADMIN"))
+				
+				.formLogin(configurer -> 
+					configurer
+						.loginPage("/showMyLoginPage")
+						.loginProcessingUrl("/authenticateTheUser")
+						.permitAll())
+				
+				.logout(configurer -> 
+					configurer
+						.permitAll())
+				
+				.exceptionHandling(configurer -> 
+					configurer
+						.accessDeniedPage("/access-denied"))
+				
+				.build();
+		}
 }
